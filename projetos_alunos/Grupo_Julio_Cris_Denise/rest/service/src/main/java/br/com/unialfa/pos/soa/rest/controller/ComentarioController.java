3cc991f4f38ba5464be208142f15d204c50ca5b9
Unknown <julio@megasoftgyn.com.br>
2017-12-16 17:56:20 -0200

    Corrigir transicao de Comentario

--- projetos_alunos/Grupo_Julio_Cris_Denise/rest/service/src/main/java/br/com/unialfa/pos/soa/rest/controller/ComentarioController.java
+++ projetos_alunos/Grupo_Julio_Cris_Denise/rest/service/src/main/java/br/com/unialfa/pos/soa/rest/controller/ComentarioController.java
@@ -6,6 +6,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -25,7 +26,8 @@ public interface ComentarioController  {
 	@PostMapping("/save-or-update")
 	public ResponseEntity<Comentario> saveOrUpdate(@RequestBody Comentario comentario, BindingResult result);
 	
-	@RequestMapping(value="/remove/{id}", method=RequestMethod.DELETE)
+	//@RequestMapping(value="/remove/{id}", method=RequestMethod.DELETE)
+	@DeleteMapping("/remove/{id}")
 	public ResponseEntity<Void> remover(@PathVariable Long id);
 	
 	
